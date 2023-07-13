5094ce367ae85833059e419dac18136366213ea0
Ashutosh5548 <mishraashutosh5548@gmail.com>
2023-07-14 02:09:16 +0530

    changes-made

--- src/pages/movieDetail/movie.js
+++ src/pages/movieDetail/movie.js
@@ -57,15 +57,6 @@ const Movie = () => {
                     
                 </div>
             </div>
-            <div className="movie__links">
-                <div className="movie__heading">Useful Links</div>
-                {
-                    currentMovieDetail && currentMovieDetail.homepage && <a href={currentMovieDetail.homepage} target="_blank" style={{textDecoration: "none"}}><p><span className="movie__homeButton movie__Button">Homepage <i className="newTab fas fa-external-link-alt"></i></span></p></a>
-                }
-                {
-                    currentMovieDetail && currentMovieDetail.imdb_id && <a href={"https://www.imdb.com/title/" + currentMovieDetail.imdb_id} target="_blank" style={{textDecoration: "none"}}><p><span className="movie__imdbButton movie__Button">IMDb<i className="newTab fas fa-external-link-alt"></i></span></p></a>
-                }
-            </div>
         </div>
     )
 }
